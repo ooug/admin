fa206d098e39b2dfa697f9a0f358c45c61fd4f3d
sandeep kumar saw <sandeepkr1084@gmail.com>
2020-09-02 18:26:48 +0530

    activities

--- src/app/pages/newsletters/newsletters.module.ts
+++ src/app/pages/newsletters/newsletters.module.ts
@@ -1,11 +1,8 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
-import {NewslettersRoutingModule} from './newsletters-routing.module';
-import {NewslettersComponent} from './newsletters.component';
-
 @NgModule({
-  declarations: [NewslettersComponent],
-  imports: [CommonModule, NewslettersRoutingModule],
+  declarations: [],
+  imports: [CommonModule],
 })
 export class NewslettersModule {}
