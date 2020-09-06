44233c2a91e9e017f5bcc9865df25d8a6d7ea93e
manraj <vmanrajsingh@gmail.com>
2020-09-06 14:43:59 -0700

    form

--- src/app/pages/gallery/gallery-routing.module.ts
+++ src/app/pages/gallery/gallery-routing.module.ts
@@ -5,6 +5,11 @@ import {GalleryComponent} from './gallery.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+
 const routes: Routes = [
   {
     path: '',
@@ -17,10 +22,18 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes),
             MatCardModule,
             MatTabsModule,
+            MatFormFieldModule,
+            MatButtonModule,
+            MatInputModule,
+            MatRadioModule
           ],
   exports: [RouterModule,
             MatCardModule,
             MatTabsModule,
+            MatFormFieldModule,
+            MatButtonModule,
+            MatInputModule,
+            MatRadioModule,
           ],
 })
 export class GalleryRoutingModule {}
